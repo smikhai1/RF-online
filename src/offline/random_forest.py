17c626b9673cd6bbe317e9bb1599cf3a499d47aa
yourhunch <sidorenko.mikhail94@gmail.com>
2019-03-19 17:11:30 +0300

    oob evaluation added

--- src/offline/random_forest.py
+++ src/offline/random_forest.py
@@ -33,13 +33,23 @@ class RandomForestClassifier(BaseEstimator):
         for t in range(self.n_estimators):
             if self.bootstrap:
                 # make bootstrap
-                X_, y_ = resample(X, y, replace=True)
+                bootstrap_idx = np.random.choice(np.arange(m), m, replace=True)
+                X_, y_ = X[bootstrap_idx], y[bootstrap_idx]
+
+                # out of bag samples
+                bag_idx = np.unique(bootstrap_idx)
+                X_oob, y_oob = np.delete(X, bag_idx, axis=0), np.delete(y, bag_idx, axis=0)
 
             # create and fit a base estimator
             base_estimator = DecisionTreeClassifier(max_depth=self.max_depth, min_samples_split=self.min_samples_split,
                                                     criterion=self.criterion, splitter=self.splitter,
                                                     max_features=self.max_features, random_state=self.random_state)
             self.estimators_.append(base_estimator.fit(X_, y_))
+
+            if self.oob_score:
+                # estimate out of bag (OOB) error for an estimator
+                y_pred = base_estimator.predict(X_oob)
+                self.oob_scores_.append(accuracy_score(y_oob, y_pred))
 
         return self
 
@@ -55,3 +65,42 @@ class RandomForestClassifier(BaseEstimator):
         y_pred, _ = mode(y_preds, axis=1)
 
         return y_pred
+
+    def refit(self, X, y, n_refitted=3):
+        m, d = X.shape
+
+        if not n_refitted:
+            n_refitted = int(self.n_estimators // 2)
+
+        if self.estimators_:
+            # delete n_reffited trees from the ensamble
+            oob_errors = 1 - np.array(self.oob_scores_)
+            p = oob_errors / np.sum(oob_errors)
+            ids = np.random.choice(np.arange(self.n_estimators), n_refitted, replace=False, p=p)
+            self.oob_scores_ = list(np.delete(self.oob_scores_, ids))
+            self.estimators_ = list(np.delete(self.estimators_, ids))
+
+
+            # fit new n_reffited trees and add to the ensamble
+            for t in range(n_refitted):
+                if self.bootstrap:
+                    # make bootstrap
+                    bootstrap_idx = np.random.choice(np.arange(m), m, replace=True)
+                    X_, y_ = X[bootstrap_idx], y[bootstrap_idx]
+
+                    # out of bag samples
+                    bag_idx = np.unique(bootstrap_idx)
+                    X_oob, y_oob = np.delete(X, bag_idx, axis=0), np.delete(y, bag_idx, axis=0)
+
+                # create and fit a base estimator
+                base_estimator = DecisionTreeClassifier(max_depth=self.max_depth,
+                                                        min_samples_split=self.min_samples_split,
+                                                        criterion=self.criterion, splitter=self.splitter,
+                                                        max_features=self.max_features, random_state=self.random_state)
+                self.estimators_.append(base_estimator.fit(X_, y_))
+
+                if self.oob_score:
+                    # estimate out of bag (OOB) error for an estimator
+                    y_pred = base_estimator.predict(X_oob)
+                    self.oob_scores_.append(accuracy_score(y_oob, y_pred))
+        return self
